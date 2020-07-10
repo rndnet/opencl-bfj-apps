9c087df8aede43d170699ae0be4ec3fafcdc5798
rndnet rndnet <rndnet@xshost11.tahiti.local>
2020-07-10 15:26:22 +0300

    Add --no-cache

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-podman build -t docker.io/rndnet/opencl-bfj-apps:microseism1d -v ${HOME}/.ssh:/root/.ssh -f Dockerfile  #use git clone git: ... for microsesm library
+podman build -t docker.io/rndnet/opencl-bfj-apps:microseism1d -v ${HOME}/.ssh:/root/.ssh   --no-cache -f Dockerfile  #use git clone git: ... for microsesm library
 #podman build -t docker.io/rndnet/opencl-bfj-apps:microseism1d                             --no-cache -f Dockerfile  #use git clone https: ... for microsesm library
