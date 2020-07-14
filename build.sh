9f65e80a1dafb34bd80a1546968ad3536b29990d
rndnet rndnet <rndnet@xshost11.tahiti.local>
2020-07-14 20:55:41 +0300

    Update base image before build

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
+podman pull docker.io/rndnet/opencl-bfj
 podman build -t docker.io/rndnet/opencl-bfj-apps:microseism3d -v ${HOME}/.ssh:/root/.ssh --no-cache -f Dockerfile  #use git clone git: ... for microsesm library
 #podman build -t docker.io/rndnet/opencl-bfj-apps:microseism3d                             --no-cache -f Dockerfile  #use git clone https: ... for microsesm library
