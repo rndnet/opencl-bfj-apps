72bcc3a91c93daf1875e043a47e04f80564f99d0
rndnet rndnet <rndnet@xshost11.tahiti.local>
2020-07-15 18:03:57 +0300

    1. Not use base image but get it Dockerfile 2. Use build date as tags postfix

---
+++ build-upload-from-scratch.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#set -eou
+
+AMD_DRV=amdgpu-pro-20.20-1089974-rhel-7.8.tar.xz
+DF=Dockerfile-scratch
+#VER=`date +"%Y-%m-%d_%H-%M-%S"`
+TAG=microseism1d
+REP=docker.io/rndnet/opencl-bfj-apps
+VER=`date +"%Y-%m-%d"`
+
+mkdir -p build/files
+
+if [ ! -f build/files/${AMD_DRV} ]; then
+    echo
+    echo "Download AMD drivers: ${AMD_DRV}"
+    wget https://media.githubusercontent.com/media/rndnet/opencl-bfj/master/files/${AMD_DRV} -O build/files/${AMD_DRV}
+fi 
+
+cd build
+
+echo
+echo "Remove FROM command from origin Dockerfile"
+sed '/^FROM /d' ../Dockerfile > Dockerfile_origin
+echo
+echo "Get base image Dockerfile"
+wget https://raw.githubusercontent.com/rndnet/opencl-bfj/master/Dockerfile -O $DF   
+echo
+echo "Create new Dockerfile: append two Dockerfiles"
+cat Dockerfile_origin  >> $DF
+cat $DF
+
+echo
+echo "Build ....."
+podman build -t ${REP}:${TAG}-${VER} -t ${REP}:${TAG} -v ${HOME}/.ssh:/root/.ssh --no-cache -f $DF  #use git clone git: ... for microsesm library
+#podman build -t ${REP}:${TAG}-${VER} -t ${REP}:${TAG}                            --no-cache -f $DF  #use git clone https: ... for microsesm library
+
+rm -rv $DF Dockerfile_origin
+
+echo
+echo Upload images
+podman push ${REP}:${TAG}-${VER} 
+podman push ${REP}:${TAG}
+
