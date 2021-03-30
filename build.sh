9cb9157cdf5ca95c75e4a0105ca05d718d5d2388
rndnet rndnet <rndnet@xshost11.tahiti.local>
2021-03-30 09:10:51 +0300

    Build in docker not oci format

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 . common
-podman build -t docker.io/rndnet/opencl-bfj-apps:${TAG} -v ${HOME}/.ssh:/root/.ssh -f Dockerfile  #use git clone git: ... for microsesm library
-#podman build -t docker.io/rndnet/opencl-bfj-apps:${TAG}                             --no-cache -f Dockerfile  #use git clone https: ... for microsesm library
+podman build -t docker.io/rndnet/opencl-bfj-apps:${TAG} -v ${HOME}/.ssh:/root/.ssh -f Dockerfile   --format $format  #use git clone git: ... for microsesm library
+#podman build -t docker.io/rndnet/opencl-bfj-apps:${TAG}                             --no-cache -f Dockerfile   --format $format  #use git clone https: ... for microsesm library
